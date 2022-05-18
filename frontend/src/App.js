84a1de30596445a4db3d5e99ee9dab1ecc78eaba
MrSix13 <bastiangarcesga@gmail.com>
2022-05-18 16:07:51 -0400

    Home screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,25 @@
+import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <a href="/">Amazona</a>
-      </header>
-      <main>List Products</main>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header>
+          <Link to="/">Amazona</Link>
+        </header>
+
+        <main>
+          <Routes>
+          <Route path="/product/:slug" element={<ProductScreen/>}/>
+            <Route path="/" element={<HomeScreen/>}/>
+          </Routes>
+          
+        </main>
+      
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/data.js
@@ -0,0 +1,45 @@
+const data ={
+    products:[
+        {
+            name: 'Call of duty: Black ops 3',
+            slug: 'Call-of-duty:-Black-ops-3',
+            category:'Shooter',
+            image:'images/p1.jpg',
+            price: 60,
+            countInStock: 10,
+            brand: 'Treyarch',
+            rating: 5,
+            numReviews:10,
+            description: 'Shooter Multiplayer'
+        
+        },
+        {
+            name: 'Call of duty: Modern Warfare',
+            slug: 'Call-of-duty:-Modern-Warfare',
+            category:'Shooter',
+            image:'images/p2.jpg',
+            price: 60,
+            countInStock: 10,
+            brand: 'Treyarch',
+            rating: 4.5,
+            numReviews:10,
+            description: 'Shooter Multiplayer'
+        
+        },
+        {
+            name: 'Call of duty: Word War 2',
+            slug: 'Call-of-duty:-WW2',
+            category:'Shooter',
+            image:'images/p3.jpg',
+            price: 60,
+            countInStock: 10,
+            brand: 'Treyarch',
+            rating: 4.5,
+            numReviews:10,
+            description: 'Shooter Multiplayer'
+        
+        }
+    ]
+}
+
+export default data;
