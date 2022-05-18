55358ceb212daa10fe8ac4a73afd1a2903a080ed
MrSix13 <bastiangarcesga@gmail.com>
2022-05-18 14:04:00 -0400

    first-react

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="/">Amazona</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
