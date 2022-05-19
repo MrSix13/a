19d41bb3fa4f946f9d9db1d21a5ea6a5b4f3b85c
MrSix13 <bastiangarcesga@gmail.com>
2022-05-18 20:23:28 -0400

    Product Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,37 @@
 import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import {LinkContainer} from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
+      <div className="d-flex flex-column site-container">
         <header>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>Amazona</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
           <Link to="/">Amazona</Link>
         </header>
 
         <main>
-          <Routes>
-          <Route path="/product/:slug" element={<ProductScreen/>}/>
-            <Route path="/" element={<HomeScreen/>}/>
-          </Routes>
-          
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen/>}/>
+              <Route path="/" element={<HomeScreen/>}/>
+            </Routes>
+          </Container>
         </main>
       
+        <footer>
+          <div className='text-center'>ALL right reserved</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,12 +1,68 @@
+import { useEffect, useReducer } from "react";
+import axios from "axios";
 import { useParams } from "react-router-dom";
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+const reducer = (state,action) =>{
+    switch(action.type){
+        case 'FETCH_REQUEST':
+            return {...state,loading:true};
+        case 'FETCH_SUCCESS':
+            return {...state,product:action.payload, loading:false};
+        case 'FETCH_FAIL':
+            return {...state, loading:false, error:action.payload};
+        default:
+            return state;
+    }
+}
+
+
 
 function ProductScreen(){
     const params = useParams();
     const {slug} = params;
 
-    return <div>
-        <h1>{slug}</h1>
-    </div>
+    const [{loading,error,product}, dispatch] = useReducer(reducer, {
+        product:[],
+        loading: true,
+        error: ''
+    })
+    //const [ products, setProducts] = useState([]);
+    
+    useEffect(() => {
+        const fetchData = async() =>{
+            dispatch({type:'FETCH_REQUEST'});
+            try {
+                const result = await axios.get(`/api/products/slug/${slug}`);
+                dispatch({type:'FETCH_SUCCESS', payload: result.data})
+            } catch (error) {
+                dispatch({type:'FETCH_FAIL', payload:error.message})
+            }
+            //setProducts(result.data);
+        };
+        fetchData();
+    }, [slug]);
+
+    return (
+        loading? (<div>Loading..</div>)
+         : error? (<div>{error}</div>)
+         : (
+             <div>
+                 <Row>
+                     <Col md={6}>
+                         <img
+                           className="img-large"
+                           src={product.image}
+                           alt={product.name}
+                         />
+                     </Col>
+                     <Col md={3}></Col>
+                     <Col md={3}></Col>
+                 </Row>
+             </div>
+         )
+    )
 }
 
 export default ProductScreen;
