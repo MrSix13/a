cc8e7bf55f250274e1dc2b7842d337b88717a967
MrSix13 <bastiangarcesga@gmail.com>
2022-09-01 16:01:12 -0400

    heroku commit

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -27,7 +27,8 @@ export default function ProfileScreen() {
     const {userInfo} = state;
     const [ name, setName] = useState(userInfo.name);
     const [email, setEmail] = useState(userInfo.email);
-    const [password, setPassword] = useState('')
+    const [password, setPassword] = useState('');
+    const [confirmPassword,setConfirmPassword] = useState('')
     const [{loadingUpdate}, dispatch] = useReducer(reducer,{
         loadingUpdate:false
     })
@@ -35,16 +36,77 @@ export default function ProfileScreen() {
     const submitHandler = async (e) =>{
         e.preventDefault();
         try {
+            const {data} = await axios.put(
+                '/api/users/profile',
+                {
+                    name,
+                    email,
+                    password
+                },
+                {
+                    headers: { Authorization: `Bearer ${userInfo.token}` },
+                },
+                dispatch({
+                    type:'UPDATE_SUCCESS'
+                })
+            );
+            ctxDispatch({type:'USER_SIGNIN', payload:data});
+            localStorage.setItem('userInfo', JSON.stringify(data));
+            toast.success('User Update Successfully')
             
         } catch (err) {
             dispatch({
                 type:'FETCH_FAIL',
             });
+            toast.error(getError(err))
         }
     }
 
     return (
-    <div>ProfileScreen</div>
+    <div className="container small-container">
+        <Helmet>
+            <title>User Profile</title>
+        </Helmet>
+        <h1 className="my-3">User Profile</h1>
+        <form onSubmit={submitHandler}>
+            <Form.Group className="mb-3" controlId='name'>
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                  value={name}
+                  onChange={(e)=>setName(e.target.value)}
+                  required
+                />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId='email'>
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  type="email"
+                  value={email}
+                  onChange={(e)=>setName(e.target.value)}
+                  required
+                />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId='password'>
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  onChange={(e)=>setPassword(e.target.value)}
+                  required
+                />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId='password'>
+                <Form.Label>Confirm Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  onChange={(e)=>setConfirmPassword(e.target.value)}
+                  required
+                />
+            </Form.Group>
+            <div className="mb-3">
+                <Button type="submit">Update</Button>
+            </div>
+        </form>
+    </div>
   )
 }
 
